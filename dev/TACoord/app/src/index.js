1c11f3478edde3791fa5d2cd078b66b01b48dcea
Atheed Thameem <atheedthameem@hotmail.com>
2017-03-11 18:25:00 -0500

    set up express, react, and directory structure for TA coord client

--- dev/TACoord/app/src/index.js
+++ dev/TACoord/app/src/index.js
@@ -1,1 +1,12 @@
-console.log("");
+"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.js';
+
+document.addEventListener('DOMContentLoaded', function() {
+    ReactDOM.render(
+        React.createElement(App),
+        document.getElementById('app-container')
+    );
+});

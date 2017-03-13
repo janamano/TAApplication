c10f43866df95aae4c517fa39c187d69ddca8680
Siddharth Gautam <siddharth.gautam@grintex.com>
2017-03-12 23:14:17 -0400

    First API call ready. Returns list of applicants.

--- dev/services/src/main.js
+++ dev/services/src/main.js
@@ -1,14 +1,20 @@
 /* --- Dependencies / libraries / frameworks --- */
 var express = require('express');
 var app = express();
-
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( {extended: true} ));
 
 // The main instance of our HTTP server
 var server = require('http').Server(app);
 
+/*DB Connection*/
+mongoose.connect("mongodb://localhost:27017/serverDB", function(err, db) {
+  if(!err) {
+    console.log("We are connected");
+  }
+});
 
 /* -- TODO: Attach request handlers for endpoints - */
 
@@ -18,7 +24,11 @@ var server = require('http').Server(app);
 var hostname = 'localhost';
 var port = 8080;
 
+var applicantsRoute = require('./router/applicant')(app);
+
+
 // Start listening for requests
 server.listen(process.env.PORT || port, function () {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+

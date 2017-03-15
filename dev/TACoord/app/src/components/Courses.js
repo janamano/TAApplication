2c65c688a9c1f58f3548d88cc2f7e91d1422fa1f
janamanoharan <jana.manoharan96@gmail.com>
2017-03-15 15:48:23 -0400

    added stuff

--- dev/TACoord/app/src/components/Courses.js
+++ dev/TACoord/app/src/components/Courses.js
@@ -15,36 +15,37 @@ export default class Courses extends Component {
 
      componentDidMount() {
         var t = this;
-         //get all the courses
-         fetch('localhost:8080/getCourseList', {method: 'GET'})
-             .then(json)
-             .then(function(data) {
-                 // store this in the state courses to create course objects
-                 const courses = data.data;
+        //  //get all the courses
+        //  fetch('localhost:8080/getCourseList', {method: 'GET'})
+        //      .then(json)
+        //      .then(function(data) {
+        //          // store this in the state courses to create course objects
+        //          const courses = data.data;
 
-                 t.setState({
-                     courses: courses
-                 });
-             })
-             .catch(function(err) {
-                // fetch didnt work
-                throw err;
-            });
+        //          t.setState({
+        //              courses: courses
+        //          });
+        //      })
+        //      .catch(function(err) {
+        //         // fetch didnt work
+        //         throw err;
+        //     });
 
         // Until then...
-       // t.setState({
-         //   courses: [
-           //     {code: "CSC108", title: "Introduction to Computer Programming"},
-             //   {code: "CSC148", title: "Introduction to Computer Science"},
-               // {code: "CSC165", title: "Mathematical Expression and Reasoning for Computer Science"},
-               // {code: "CSC207", title: "Software Design"},
-           // ]
-       // });
+       t.setState({
+           courses: [
+               {code: "CSC108", title: "Introduction to Computer Programming", numberOfTAs: 40, qualifications: "CSC108"},
+               {code: "CSC148", title: "Introduction to Computer Science", numberOfTAs: 40, qualifications: "CSC108, CSC148"},
+               {code: "CSC165", title: "Mathematical Expression and Reasoning for Computer Science", numberOfTAs: 40, qualifications: "CSC165"},
+               {code: "CSC207", title: "Software Design",numberOfTAs: 40, qualifications: "CSC207"},
+           ]
+       });
     }
    
     courseList() {
       this.state.courses.map(function(course) {
-        return <Course code={course.code} title={course.title} />
+        return <Course code={course.code} title={course.title}  numberOfTAs={course.numberOfTAs}
+                            qualifications={course.qualifications}/>
       });
     }
 

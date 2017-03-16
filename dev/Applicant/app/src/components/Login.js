4f03868d060488a5e941e6e7ca30420a0b24a43f
janamanoharan <jana.manoharan96@gmail.com>
2017-03-16 08:48:19 -0400

    added validation front end

--- dev/Applicant/app/src/components/Login.js
+++ dev/Applicant/app/src/components/Login.js
@@ -8,12 +8,14 @@ export default class Login extends Component {
     
     render() {
         return (
+            <form>
             <Row>
-                <Input label="User Name" s={6} />
-                <Input label="Password" s={6} type="password" />
-                <Input label="Email" s={12} type="email"/>
+                <Input class='validate' pattern='[a-zA-Z0-9].{5,}' label="UtorID" s={6} required/>
+                <Input class='validate' pattern='[0-9].{10}' label="Student Number" s={6} required/>
+                
                 <Button waves='light' type="submit">Login</Button>
             </Row>
+            </form>
         );
 
     }

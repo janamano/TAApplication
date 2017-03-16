140cb577e071ec4587132dca880781be84a1703e
janamanoharan <jana.manoharan96@gmail.com>
2017-03-16 09:40:02 -0400

    added better validation

--- dev/Applicant/app/src/components/Login.js
+++ dev/Applicant/app/src/components/Login.js
@@ -6,14 +6,31 @@ export default class Login extends Component {
         super();
     }
     
+    handleClick(e) {
+        e.preventDefault();
+
+        var utoridregex = /^([A-Za-z0-9]{5,})$/;
+        var stunumregex = /^([0-9]{6,})$/;
+
+        if (! utoridregex.test(document.getElementById("utorid").value)) {
+            Materialize.toast('Enter a valid UtorID', 2000);
+        }
+         
+        if (! stunumregex.test(document.getElementById("studentnum").value)) {
+          Materialize.toast('Enter a valid Student Number', 2000);
+        }
+
+        // hook to back end
+    }
+
     render() {
         return (
             <form>
             <Row>
-                <Input class='validate' pattern='[a-zA-Z0-9].{5,}' label="UtorID" s={6} required/>
-                <Input class='validate' pattern='[0-9].{10}' label="Student Number" s={6} required/>
+                <Input id="utorid" class='validate' pattern='[a-zA-Z0-9].{5,}' label="UtorID" s={6} required/>
+                <Input id="studentnum" class='validate' pattern='[0-9].{10}' label="Student Number" s={6} required/>
                 
-                <Button waves='light' type="submit">Login</Button>
+                <Button waves='light' onClick={this.handleClick} type="submit">Login</Button>
             </Row>
             </form>
         );

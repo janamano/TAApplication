658efcfb3fde7fb5cedf98907e4459205fa4f2f7
Siddharth Gautam <siddharth.gautam@grintex.com>
2017-03-18 16:59:56 -0400

    Added base saveApplication API route.

--- dev/services/src/router/application.js
+++ dev/services/src/router/application.js
@@ -23,5 +23,43 @@ module.exports = function(app) {
         	}
         });
     });
+    app.post('/saveApplication/', function(req, res) {
+        var utorId = req.body.utorid;
+        var session = req.body.session;
+        var status = req.body.status;
+        var rankings = req.body.rankings;
+        console.log(rankings);
+        res.send("RECVD");
+        /*
+        rankings: {
+            1: ["CSC165"],
+            2: ["CSC108", "CSC324"],
+            3: ["CSC443"],
+            4: [],
+            5: ["CSC207", "CSC236"],
+            0: []
+            }
+        */ 
+
+        /*
+        applications.find({UTORid: utorId}, function(err, application){
+        	if (err) {
+                res.status(400)
+                    .json({
+                        status: 'error',
+                        data: {},
+                        message: err
+                    });
+        	} else {
+        		console.log("Sending resp");
+                res.status(200)
+                    .json({
+                        status: 'error',
+                        data: application,
+                        message: "Successfully found all applicants"
+                    });
+        	}
+        });*/
+    });
    
 };

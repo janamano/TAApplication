0a468d59c3c1b4289a950b9474565f5ac9c668d6
Sajid Ahmed <sajid.ahmed@mail.utoronto.ca>
2017-04-06 20:31:35 -0400

    Added tests for GET /openings

--- dev/services/test/CoursesTest.js
+++ dev/services/test/CoursesTest.js
@@ -5,37 +5,80 @@ chai.should();
 
 chai.use(chaiHttp);
 
+function checkBasicStructure(response) {
+  
+  response.should.have.status(200);
+  
+  response.should.be.json;
+  response.body.should.be.a('object');
+  
+  response.body.should.have.property('status');
+  response.body.status.should.equal('success');
+  
+  response.body.should.have.property('data');
+  response.body.data.should.be.a('array');
+}
+
+function checkCourseAttributes(expected, actual) {
+  actual.should.have.property('_id');
+  actual.should.have.property('code');
+  actual.should.have.property('title');
+  actual.should.have.property('instructor');
+  actual.should.have.property('numberOfTAs');
+  actual.should.have.property('qualifications');
+  actual.should.have.property('__v');
+  
+  // These value checks will be of more use once we use 'hooks' to
+  // populate the database with test data before running each test
+  actual.code.should.equal(expected.code);
+  actual.title.should.equal(expected.title);
+  actual.instructor.should.equal(expected.instructor);
+  actual.numberOfTAs.should.equal(expected.numberOfTAs);
+  actual.qualifications.should.equal(expected.qualifications);
+}
+
+
+function assert(response, expectedData, actualData) {
+  checkBasicStructure(response);
+  checkCourseAttributes(expectedData, actualData);
+}
+
 describe('Courses', function() {
   it('GET /courseList returns list of all courses', function(done) {
-    chai.request(server)
-    .get('/getCourseList')
-    .end(function(err, res) {
-      res.should.have.status(200);
-      res.should.be.json;
-      res.body.should.be.a('object');
+    
+    chai.request(server).get('/getCourseList').end(function(error, response) {
       
-      res.body.should.have.property('status');
-      res.body.status.should.equal('success');
+      expected = {
+        code: "CSC207",
+        title: "Software Design",
+        instructor: "Anya Tafliovich",
+        numberOfTAs: 20,
+        qualifications: "Excellent OO skills nad Experience with Java",
+        __v: 0
+      };
       
-      res.body.should.have.property('data');      
-      res.body.data.should.be.a('array');
+      actual = response.body.data[0];      
+      assert(response, expected, actual);
       
-      data = res.body.data;
-      data[0].should.have.property('_id');
-      data[0].should.have.property('code');
-      data[0].should.have.property('title');
-      data[0].should.have.property('instructor');
-      data[0].should.have.property('numberOfTAs');
-      data[0].should.have.property('qualifications');
-      data[0].should.have.property('__v');
+      done();
+    });
+  });
+  
+  it('GET /getOpenings returns list of all unassigned TA positions', function(done) {
+    
+    chai.request(server).get('/getOpenings').end(function (error, response) {
       
-      // These value checks will be of more use once we use 'hooks' to
-      // populate the database with test data before running each test
-      data[0].code.should.equal('CSC207');
-      data[0].title.should.equal('Software Design');
-      data[0].instructor.should.equal('Anya Tafliovich');
-      data[0].numberOfTAs.should.equal(20);
-      data[0].qualifications.should.equal('Excellent OO skills nad Experience with Java');
+      expected = {
+        code: "CSC148",
+        title: "Introduction to Computer Science",
+        instructor: "Pit Franc",
+        numberOfTAs: 20,
+        qualifications: "Obtained at least A in CSC108 and CSC148",
+        __v: 0
+      };
+      
+      actual = response.body.data[1];
+      assert(response, expected, actual);
       done();
     });
   });

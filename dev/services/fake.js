f94cf563a12523c9f6faea46abd58ed3f500142b
gabriellesc <gabrielle.singhcadieux@mail.utoronto.ca>
2017-04-08 19:25:05 -0400

    moved populating/cleaning functions to utils file + use promises to correctly order functions in fake.js

--- dev/services/fake.js
+++ dev/services/fake.js
@@ -1,4 +1,6 @@
-var util = require('./src/test/test-utils');
+process.env.NODE_ENV = 'development'; // dev environment by default
+
+var util = require('./test/test-utils');
 var fs = require('fs');
 
 var mongoose = require('mongoose');
@@ -10,61 +12,85 @@ var Course = require('./models/Courses');
 var Application = require('./models/Application');
 var Assignment = require('./models/Assignment');
 
-var applicantFile = './src/test/applicants.json';
-var courseFile = './src/test/courses.json';
-var applicationFile = './src/test/applications.json';
-var assignmentFile = './src/test/assignments.json';
+var applicantFile = './test/testData/applicants.json';
+var courseFile = './test/testData/courses.json';
+var applicationFile = './test/testData/applications.json';
+var assignmentFile = './test/testData/assignments.json';
 
-// populate DB
-if (process.argv[2] == '-p') {
 
-    console.log('Applicant population in progress');
+var populate = function(func){
+    
     var data = fs.readFileSync(applicantFile);
     var applicants = JSON.parse(data);
 
-    var i;
-    for (i = 0; i < applicants.length; i++)
-	util.addApplicant(applicants[i]);
-
-    console.log('Course population in progress');
     var data = fs.readFileSync(courseFile);
     var courses = JSON.parse(data);
 
-    var i;
-    for (i in courses)
-	util.addCourse(courses[i]);
-    
-    console.log('Application population in progress');
     var data = fs.readFileSync(applicationFile);
     var applications = JSON.parse(data);
 
-    var i;
-    for (i = 0; i < applications.length; i++)
-	util.addApplication(applications[i]);
-
-    console.log('Assignment population in progress');    
     var data = fs.readFileSync(assignmentFile);
     var assignments = JSON.parse(data);
 
-    var i, a;
-    for (i = 0; i < assignments.length; i++){
-	// get the corresponding course from the course data
-	a = assignments[i];
-	a.assignedCourse = courses[a.assignedCourse];
-	
-	util.addAssignment(a);
-    }
-
-    console.log('Process Complete!');
     
-    // clean DB
-} else if (process.argv[2] == '-c'){
+    util.addApplicants(
+	0, applicants, util.addCourses(
+		0, courses, util.addApplications(
+			0, applications, util.addAssignments(
+			    0, assignments, func))));
+};
 
-    console.log('Cleaning database');
-    util.cleanDB();
-    console.log('Process Complete!');
-}
+var clean = function(func){
+    util.cleanDB(func);
+};
 
-// do not persist connection to DB
-if (process.argv.indexOf('-ps') == -1)
+// process command-line arguments
+
+if (process.argv.indexOf('--help') != -1 || process.argv.indexOf('-h') != -1) {
+    console.log('Usage: fake.js [-t] [-ps] [-p] [-c]\n' +
+		'-t \tuse testing environment (dummy database)\n' +
+		'-ps \tpersist database connection after running script\n' +
+		'-p \tpopulate database with fake data\n' +
+		'-c \tclean database');
+
     db.disconnect();
+    
+} else {
+    // use testing environment
+    if (process.argv.indexOf('-t') != -1)
+	process.env.NODE_ENV = 'test';
+
+    var p = process.argv.indexOf('-p') != -1;
+    var c = process.argv.indexOf('-c') != -1;
+    
+    // persist connection to DB
+    var ps = process.argv.indexOf('-ps') != -1;
+
+    if (p) {
+	// populate and then clean DB
+	if (c) {
+
+	    // persist connection to DB
+	    if (ps)
+		populate(() => clean());
+	    else
+		populate(() => clean(() => db.disconnect()));
+	    
+	// populate DB and persist connection
+	} else if (ps) {
+	    populate(clean);
+
+	// just populate DB
+	} else {
+	    populate(() => db.disconnect());
+	}
+	
+    } else if (c) {
+	// clean DB and persist connection
+	if (ps)
+	    clean();
+	// just clean DB
+	else
+	    clean(() => db.disconnect());
+    }
+}
--- dev/services/test/test-utils.js
+++ dev/services/test/test-utils.js
@@ -8,33 +8,107 @@ var Course = require('../models/Courses');
 var Application = require('../models/Application');
 var Assignment = require('../models/Assignment');
 
+// add single applicant to DB
 exports.addApplicant = function(a, func){
-    new Applicant(a).save(func);
+    new Applicant(a).save().then(func);
 };
 
+// add applicants recursively to DB
+exports.addApplicants = function(i, applicants, func){
+    if (i < applicants.length){
+
+	// if we have added all applicants, call the callback function
+	if (i == applicants.length-1)
+	    exports.addApplicant(applicants[i], func);
+
+	else
+	    exports.addApplicant(applicants[i], function(){
+		exports.addApplicants(i+1, applicants, func);
+	    });
+    }
+};
+
+// add single course to DB
 exports.addCourse = function(a, func){
-    new Course(a).save(func);
+    new Course(a).save().then(func);
 };
 
+// add courses recursively to DB       
+exports.addCourses = function(i, courses, func){
+
+    var courseIds = Object.keys(courses);
+    
+    (function recAdd(j){
+	if (j < courseIds.length){
+
+	    // if we have added all courses, call the callback function
+	    if (j == courseIds.length-1)
+		exports.addCourse(courses[courseIds[j]], func);
+
+	    else
+		exports.addCourse(courses[courseIds[j]], function(){
+		    recAdd(j+1);
+		});
+	}
+    })(i);
+};
+
+// add single application to DB
 exports.addApplication = function(a, func){
-    new Application(a).save(func);
+    new Application(a).save().then(func);
 };
 
+// add applications recursively to DB
+exports.addApplications = function(i, applications, func){
+    if (i < applications.length){
+
+	// if we have added all applications, call the callback function
+	if (i == applications.length-1)
+	    exports.addApplication(applications[i], func);
+
+	else
+	    exports.addApplication(applications[i], function(){
+		exports.addApplications(i+1, applications, func);
+	    });
+    }
+};
+
+// add single assignment to DB
 exports.addAssignment = function(a, func){
-    new Assignment(a).save(func);
+    new Assignment(a).save().then(func);
 };
 
+// add assignments recursively to DB
+exports.addAssignments = function(i, assignments, func){
+    if (i < assignments.length){
+
+	// if we have added all assignments, call the callback function
+	if (i == assignments.length-1)
+	    exports.addAssignment(assignments[i], func);
+
+	else
+	    exports.addAssignment(assignments[i], function(){
+		exports.addAssignments(i+1, assignments, func);
+	    });
+    }
+};
+
+// clean DB collection
 exports.cleanCollection = function(c, func){
-    c.remove({}).exec(func);
+    c.remove({}).exec().then(func);
 };
 
-exports.cleanDB = function(){
-    exports.cleanCollection(Applicant);
-    exports.cleanCollection(Course);
-    exports.cleanCollection(Application);
-    exports.cleanCollection(Assignment);
+// clean all collections of DB
+exports.cleanDB = function(func){
+    exports.cleanCollection(
+	Applicant, () => exports.cleanCollection(
+	    Course, () => exports.cleanCollection(
+		Application, () => exports.cleanCollection(
+		    Assignment, () => func()
+		))));
 };
 
+// expect course a to have same properties and values as course b
 exports.compareCourses = function(a, b){
     expect(a).to.have.property('code', b.code);
     expect(a).to.have.property('title', b.title);
@@ -43,6 +117,7 @@ exports.compareCourses = function(a, b){
     expect(a).to.have.property('qualifications', b.qualifications);
 };
 
+// expect applicant a to have same properties and values as applicant b
 exports.compareApplicants = function(a, b){
     expect(a).to.have.property('studentNumber', parseInt(b.studentNumber));
     expect(a).to.have.property('UTORid', b.UTORid);

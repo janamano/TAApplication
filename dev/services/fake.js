43ccbc8d8abbd02b4f9215aa814eeec6d63199bf
gabriellesc <gabrielle.singhcadieux@mail.utoronto.ca>
2017-04-10 02:57:04 -0400

    fixed a bug in fake script

--- dev/services/fake.js
+++ dev/services/fake.js
@@ -43,7 +43,10 @@ var populate = function(func){
 
     var data = fs.readFileSync(assignmentFile);
     var assignments = JSON.parse(data);
-
+    // match courseID in assignment to course itself
+    var i;
+    for (i = 0; i < assignments.length; i++)
+	assignments.assignedCourse = courses[assignments.assignedCourse];
     
     util.addApplicants(
 	0, applicants, util.addCourses(
